e169cf1247e844ccb303a79f62c3519d749b285d
Xuehan <xxman@google.com>
2025-06-28 23:05:36 +0000

    change pass@1 to pass@k in configs.
    Signed-off-by: Xuehan <xxman@google.com>

--- nemo_rl/environments/math_environment.py
+++ nemo_rl/environments/math_environment.py
@@ -106,6 +106,7 @@ class MathVerifyWorker:
                 results.append((0.0, extracted_answer))
         return results
 
+
 @ray.remote
 class MultichoiceVerifyWorker:
     def verify(
@@ -122,7 +123,7 @@ class MultichoiceVerifyWorker:
         """
         results = []
         for response, ground_truth in zip(pred_responses, ground_truths):
-            response = normalize_response(response)
+            response = answer_parsing.normalize_response(response)
             extracted_answer = None
             for answer_regex in answer_parsing.MULTILINGUAL_ANSWER_REGEXES:
                 regex = answer_parsing.MULTILINGUAL_ANSWER_PATTERN_TEMPLATE.format(
