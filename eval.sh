68c78a59d2d5d973fcbc780a59ec88bf8621b93c
Xuehan <xxman@google.com>
2025-07-14 22:28:32 +0000

    add a script to run eval on all datasets.
    Signed-off-by: Xuehan <xxman@google.com>

---
+++ eval.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Check for exactly 2 arguments
+if [ "$#" -ne 2 ]; then
+    echo "Usage: $0 <ckpt_path> <exp_name>"
+    exit 1
+fi
+
+ckpt_path=$1
+exp_name=$2
+
+benchmarks=("gpqa" "math" "math500" "mgsm" "mmlu" "mmlu_pro")
+
+for benchmark_name in "${benchmarks[@]}"; do
+  if [ $benchmark_name = "math500" ]; then
+    config_file="examples/configs/evals/math.yaml"  
+  else
+    config_file="examples/configs/evals/${benchmark_name}.yaml"
+  fi
+
+  echo "Reading from config: ${config_file}"
+  wandb_name="$exp_name-$benchmark_name"
+  if [ $benchmark_name = "math500" ]; then
+    uv run examples/run_eval.py --config $config_file data.dataset_name="math500" cluster.gpus_per_node=8 generation.model_name=$ckpt_path logger.wandb.name=$wandb_name
+  else
+    uv run examples/run_eval.py --config $config_file cluster.gpus_per_node=8 generation.model_name=$ckpt_path logger.wandb.name=$wandb_name
+  fi
+done
