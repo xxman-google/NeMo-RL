0101770762f51a89c844b669ae8382af6fd7c58e
Xuehan <xxman@google.com>
2025-07-18 20:22:23 +0000

    add AIME2024 to batch eval script.
    Signed-off-by: Xuehan <xxman@google.com>

--- eval.sh
+++ eval.sh
@@ -8,8 +8,11 @@ fi
 
 ckpt_path=$1
 exp_name=$2
+hf_ckpt_path=$ckpt_path/hf
 
-benchmarks=("gpqa" "math" "math500" "mgsm" "mmlu" "mmlu_pro")
+uv run python examples/converters/convert_dcp_to_hf.py --config $ckpt_path/config.yaml --dcp-ckpt-path $ckpt_path/policy/weights/ --hf-ckpt-path $hf_ckpt_path
+
+benchmarks=("aime2024" "gpqa" "math" "math500" "mgsm" "mmlu" "mmlu_pro")
 
 for benchmark_name in "${benchmarks[@]}"; do
   if [ $benchmark_name = "math500" ]; then
@@ -21,8 +24,8 @@ for benchmark_name in "${benchmarks[@]}"; do
   echo "Reading from config: ${config_file}"
   wandb_name="$exp_name-$benchmark_name"
   if [ $benchmark_name = "math500" ]; then
-    uv run examples/run_eval.py --config $config_file data.dataset_name="math500" generation.stop_token_ids=\[151643,151645\] cluster.gpus_per_node=8 generation.model_name=$ckpt_path logger.wandb.name=$wandb_name
+    uv run examples/run_eval.py --config $config_file data.dataset_name="math500" generation.stop_token_ids=\[151643,151645\] cluster.gpus_per_node=8 generation.model_name=$hf_ckpt_path logger.wandb.name=$wandb_name
   else
-    uv run examples/run_eval.py --config $config_file generation.stop_token_ids=\[151643,151645\] cluster.gpus_per_node=8 generation.model_name=$ckpt_path logger.wandb.name=$wandb_name
+    uv run examples/run_eval.py --config $config_file generation.stop_token_ids=\[151643,151645\] cluster.gpus_per_node=8 generation.model_name=$hf_ckpt_path logger.wandb.name=$wandb_name
   fi
 done
