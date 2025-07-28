7db767b2089d5e23980abebf28a43218ddbbddad
Xuehan <xxman@google.com>
2025-07-28 16:37:31 +0000

    log token length histogram in `run_create_htmls.py`.
    Signed-off-by: Xuehan <xxman@google.com>

--- examples/run_create_htmls.py
+++ examples/run_create_htmls.py
@@ -2,6 +2,7 @@
 
 from absl import app, flags
 from datasets import load_dataset
+from transformers import AutoTokenizer
 
 from nemo_rl.evals import visualization as vis_lib
 from nemo_rl.utils.logger import Logger, LoggerConfig, WandbConfig
@@ -16,6 +17,7 @@ _DATASET_NAME = flags.DEFINE_string("dataset_name", None, "Dataset name.")
 _WHICH_SAMPLES = flags.DEFINE_string(
     "which_samples", "first_30", "`first_n`, `last_n`, or `random_n`"
 )
+_TOKENIZER = flags.DEFINE_string("tokenizer", "Qwen/Qwen3-14B", "Tokenizer name.")
 
 
 def get_prompt_and_response(messages: list[dict[str, str]]) -> tuple[str, str]:
@@ -34,10 +36,11 @@ def main(_):
         log_dir=_WANDB_LOG_DIR.value,
         wandb_enabled=True,
         tensorboard_enabled=False,
-        wandb=WandbConfig(project="data", name=_DATASET_NAME.name),
+        wandb=WandbConfig(project="data", name=_DATASET_NAME.value),
         monitor_gpus=False,
     )
     logger = Logger(logger_config)
+    tokenizer = AutoTokenizer.from_pretrained(_TOKENIZER.value)
     ds = load_dataset("parquet", data_files=_INPUT_PARQUET_FILES.value, split="train")
     location, num_samples = _WHICH_SAMPLES.value.split("_")
     num_samples = int(num_samples)
@@ -51,6 +54,7 @@ def main(_):
     else:
         raise ValueError(f"Invalid arg for `which_samples` {_WHICH_SAMPLES.value}.")
     htmls = []
+    generation_lengths = []
     for msg in messages:
         prompt, response = get_prompt_and_response(msg)
         html = vis_lib.MathRenderTemplate().render(
@@ -61,9 +65,13 @@ def main(_):
             extracted_answer=None,
         )
         htmls.append(html)
+        token_ids = tokenizer(response, return_tensors="np")["input_ids"][0]
+        generation_lengths.append(len(token_ids))
 
     all_htmls = vis_lib.make_report_from_example_htmls(htmls)
     logger.log_html("Data", all_htmls)
+    logger.log_histogram("generation length", generation_lengths, num_bins=10)
+    print("average token length: ", sum(generation_lengths) / len(generation_lengths))
 
 
 if __name__ == "__main__":
