b30e4cfdfde24140955ede99671eda3d9a3cb70a
Xuehan <xxman@google.com>
2025-08-12 05:41:38 +0000

    add livecodebench to eval.sh.
    Signed-off-by: Xuehan <xxman@google.com>

--- eval.sh
+++ eval.sh
@@ -7,7 +7,7 @@ if [ "$#" -ne 2 ]; then
 fi
 
 enable_thinking=false
-if $enable_thinking; then
+if [[ $enable_thinking == "true" ]]; then
   max_model_len=38912
   temperature=0.6
   top_p=0.95
@@ -23,13 +23,11 @@ top_k=20
 
 ckpt_path=$1
 exp_name=$2
-hf_ckpt_path=$ckpt_path
-#hf_ckpt_path=$ckpt_path/hf
+hf_ckpt_path=$ckpt_path/hf
 
-# uv run python examples/converters/convert_dcp_to_hf.py --config $ckpt_path/config.yaml --dcp-ckpt-path $ckpt_path/policy/weights/ --hf-ckpt-path $hf_ckpt_path
+uv run python examples/converters/convert_dcp_to_hf.py --config $ckpt_path/config.yaml --dcp-ckpt-path $ckpt_path/policy/weights/ --hf-ckpt-path $hf_ckpt_path
 
-benchmarks=("aime2024" "aime2025" "gpqa" "math" "math500" "mgsm" "mmlu" "mmlu_pro")
-# benchmarks=("aime2025" "mmlu" "mmlu_pro")
+benchmarks=("aime2024" "aime2025" "beyond_aime" "gpqa" "math" "math500" "mgsm" "mmlu" "mmlu_pro" "humaneval" "livecodebench_functional" "livecodebench_stdin")
 
 for benchmark_name in "${benchmarks[@]}"; do
   if [ $benchmark_name = "math500" ]; then
@@ -37,34 +35,25 @@ for benchmark_name in "${benchmarks[@]}"; do
   else
     config_file="examples/configs/evals/${benchmark_name}.yaml"
   fi
+  if [[ $benchmark_name == "livecodebench"* ]]; then
+    dataset_name="livecodebench"
+  else
+    dataset_name=$benchmark_name
+  fi
 
   echo "Reading from config: ${config_file}"
   wandb_name="$exp_name-$benchmark_name"
-  if [ $benchmark_name = "math500" ]; then
-    uv run examples/run_eval.py --config $config_file \
-      cluster.gpus_per_node=8 \
-      data.dataset_name="math500" \
-      eval.num_tests_per_prompt=$num_tests_per_prompt \
-      generation.top_p=$top_p \
-      generation.top_k=$top_k \
-      generation.model_name=$hf_ckpt_path \
-      generation.temperature=$temperature \
-      generation.stop_token_ids=\[151643,151645\] \
-      generation.enable_thinking=$enable_thinking \
-      generation.vllm_cfg.max_model_len=$max_model_len \
-      logger.wandb.name=$wandb_name
-  else
-    uv run examples/run_eval.py --config $config_file \
-      cluster.gpus_per_node=8 \
-      data.dataset_name=$benchmark_name \
-      eval.num_tests_per_prompt=$num_tests_per_prompt \
-      generation.top_p=$top_p \
-      generation.top_k=$top_k \
-      generation.model_name=$hf_ckpt_path \
-      generation.temperature=$temperature \
-      generation.stop_token_ids=\[151643,151645\] \
-      generation.enable_thinking=$enable_thinking \
-      generation.vllm_cfg.max_model_len=$max_model_len \
-      logger.wandb.name=$wandb_name
-  fi
+
+  uv run examples/run_eval.py --config $config_file \
+  data.dataset_name=$dataset_name \
+  generation.stop_token_ids=\[151643,151645\] \
+  generation.enable_thinking=$enable_thinking \
+  generation.vllm_cfg.max_model_len=$max_model_len \
+  generation.temperature=$temperature \
+  generation.top_p=$top_p \
+  generation.top_k=$top_k \
+  cluster.gpus_per_node=$GPUS_PER_NODE \
+  cluster.num_nodes=$NNODES \
+  generation.model_name=$hf_ckpt_path \
+  logger.wandb.name=$wandb_name
 done
