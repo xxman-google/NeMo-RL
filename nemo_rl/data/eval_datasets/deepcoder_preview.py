35679ad56a7ab26c0d816173f77a0c72f84d6923
Fei Xia <feixia@google.com>
2025-09-03 23:57:52 +0000

    add length filter for code tests

--- nemo_rl/data/eval_datasets/deepcoder_preview.py
+++ nemo_rl/data/eval_datasets/deepcoder_preview.py
@@ -10,6 +10,7 @@ from nemo_rl.data import processors
 from nemo_rl.data.interfaces import TaskDataSpec
 from typing import Any, Literal, Optional
 
+_TEST_LENGTH_LIMIT = 50000000
 
 class DeepCoderPreviewDataset:
     def __init__(
@@ -23,16 +24,11 @@ class DeepCoderPreviewDataset:
         ds = load_dataset("agentica-org/DeepCoder-Preview-Dataset", 
                           name=subset,
                           split="train")
+        ds = ds.filter(lambda x: len(x["tests"]) < _TEST_LENGTH_LIMIT)
         if subset == "lcbv5":
-            def _filter_lcbv5(example: dict[str, Any]):
-                tests = json.loads(example["tests"])
-                return len(tests) > 0 and tests[0]["testtype"] == "stdin"
-            ds = ds.filter(_filter_lcbv5)
+            ds = ds.filter(self._filter_lcbv5)
         elif subset == "primeintellect":
-            def _filter_primeintellect(example: dict[str, Any]):
-                tests = json.loads(example["tests"])
-                return len(tests) > 0 and tests[0]["type"] == "stdin_stdout"
-            ds = ds.filter(_filter_primeintellect)
+            ds = ds.filter(self._filter_primeintellect)
 
         self.rekeyed_ds = ds.map(self._rekey, remove_columns=ds.column_names)
         self.task_spec = TaskDataSpec(
@@ -49,3 +45,11 @@ class DeepCoderPreviewDataset:
             "tests": json.loads(example["tests"]),
             "code_exe_dir": self.code_exe_dir,
         }
+    
+    def _filter_lcbv5(self, example: dict[str, Any]):
+        tests = json.loads(example["tests"])
+        return len(tests) > 0 and tests[0]["testtype"] == "stdin"
+    
+    def _filter_primeintellect(self, example: dict[str, Any]):
+        tests = json.loads(example["tests"])
+        return len(tests) > 0 and tests[0]["type"] == "stdin_stdout"
