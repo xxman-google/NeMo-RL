97e0717348ba300bb978639240baa22584d0e8ef
Evelyn Walker <evelynwalker@google.com>
2025-09-04 15:35:38 -0700

    Update dedup.sh
    Signed-off-by: Evelyn Walker <evelynwalker@google.com>

--- dedup.sh
+++ dedup.sh
@@ -1,7 +1,7 @@
 # if certain packages are not installed, run:
 # pip install sentence-transformers scikit-learn matplotlib pandas seaborn
 
-uv run /examples/dedup_run_pip.py \
+uv run /examples/run_dedup.py \
 --data_path /data/cirrus0.0/openr1_math_amc_aime_qwen3_8b_no_thinking/train-unboxed-00000-of-00001.parquet \
 --output_dir ./dedup \
 --model_name all-MiniLM-L6-v2 \
